e34f35b102eb6704f4f50f306ec47a13fe99331c
lextri980 <lextri980@gmail.com>
2022-10-26 21:42:57 +0700

    update file name

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { login, register } = require("../controllers/auth.cts");
+const { login, register } = require("../controllers/auth.controller");
 
 router.post("/login", login);
 router.post("/register", register);
