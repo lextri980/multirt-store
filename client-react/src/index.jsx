ae42c48cc6f186c5e88846afeb0b7e1a0d7062e4
lextri980 <lextri980@gmail.com>
2022-11-14 20:57:49 +0700

    push url saga

--- client-react/src/index.jsx
+++ client-react/src/index.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-// import { HistoryRouter as Router } from "redux-first-history/rr6";
+import { HistoryRouter as Router } from "redux-first-history/rr6";
 import App from "./App";
 import { history, store } from "./store/store";
 import "./themes/scss/index.scss";
